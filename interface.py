4da872626d5bad6e7016e06a56dfcfe006675aff
Komal Dheer <dheerkomal8@gmail.com>
2026-08-24 11:07:02 +0530

    Implement model loading from URL in load_model function
    Added a new load_model function to download and load a model from a URL using requests and pickle. Updated the previous load_model function to be commented out.

--- interface.py
+++ interface.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 import pandas as pd
 import streamlit as st
+import pickle
+import requests
+import os
 from PIL import Image
 from skimage.feature import hog
 from skimage.exposure import rescale_intensity
@@ -54,8 +57,8 @@ else:
 
 
 @st.cache_resource
-def load_model(path):
-    return joblib.load(path)
+# def load_model(path):
+#     return joblib.load(path)
 
 def model_probabilities(model, features):
     if hasattr(model, "predict_proba"):
@@ -72,6 +75,27 @@ def predict_image(image, project, model_name):
     )
     prediction = model.predict([features])[0]
     return prediction, model_probabilities(model, features)
+#====================================================================================================
+@st.cache_resource
+def load_model(path):
+    url = "https://github.com/KomlDheer/shape_recognition/releases/tag/v1.0.0"
+    model_path = "path"
+    
+    try:
+        response = requests.get(url)
+        # Check if the HTTP request was successful (Status 200)
+        if response.status_code == 200:
+            with open(model_path, "wb") as f:
+                f.write(response.content)
+            
+            with open(model_path, "rb") as f:
+                return pickle.load(f)
+        else:
+            st.error(f"Failed to download model. HTTP Status: {response.status_code}")
+            return None
+    except Exception as e:
+        st.error(f"Error loading model: {e}")
+        return None
 
 
 image_input = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
